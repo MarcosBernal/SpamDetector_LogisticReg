cd1fd87c67e404ae04bfae0f9e0d12311ce6751a
MarcosBernal <marcosbernal3@hotmail.com>
2017-12-16 15:36:41 +0100

    Added spyder3 desktop container (docker) with pyspark

---
+++ spyder_spark.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# Need sudo access to use docker
+echo "sudo access required"
+sudo echo ""
+
+# Moving to the directory of the script
+cd $(pwd)/$(dirname $0)
+
+xhost local:root
+
+sudo docker run --rm \
+    -i -t \
+    -u root \
+    -v $(pwd)/$(dirname $0)/notebook:/home/dev \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+    -e DISPLAY=unix$DISPLAY \
+    spyder-jupyter-pyspark spyder3 -w /home/dev
