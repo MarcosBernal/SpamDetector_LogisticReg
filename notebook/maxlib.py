8bc673b332ab72173a3e377c913b1a76869c16ee
Massimiliano R <mrusso.pro@gmail.com>
2017-12-27 00:57:07 +0100

    Minor changes

--- notebook/maxlib.py
+++ notebook/maxlib.py
@@ -10,6 +10,15 @@ import math, time
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Normalize values
+def normalize_data(data):
+    max_min = data.flatMap(lambda x: [ (index_key, x[index_key]) for index_key in range(len(x)-1)]) #Last position is label
+    max__list = sorted(max_min.reduceByKey(lambda x,y: x if x > y else y).collect())
+    min__list = sorted(max_min.reduceByKey(lambda x,y: x if x < y else y).collect())
+    mean_list = sorted([ value[1]/data.count() for value in max_min.reduceByKey(lambda x,y: x + y).collect()])
+    
+    return data.map(lambda x: [(float(x[index]) - min__list[index][1])/(max__list[index][1] - min__list[index][1]) if index != len(x)-1 else x[index] for index in range(len(x))] )   
+
 #def reduce_features(data_rdd):
 #    
 #    is_to_keep = [6:13] + [15:18] + [22:27] + [29, 31, 40, 42, 45, 46, 51] + [53:57] 
